3903d6bc493002eca22bcd4029354be3664c7ac2
Francesca Garbagnati <f.g.garbagnati@gmail.com>
2017-01-23 12:58:07 -0800

    Enables user input and drag on pet

--- public/js/main.js
+++ public/js/main.js
@@ -21,6 +21,8 @@ var GameState = {
 		this.pet = this.game.add.sprite(100, 400, 'pet');
 		this.pet.anchor.setTo(0.5);
 		this.pet.customParams = {health: 100, fun: 100};
+		this.pet.inputEnabled = true;
+		this.pet.input.enableDrag();
 
 		this.apple = this.game.add.sprite(72, 570, 'apple');
 		this.candy = this.game.add.sprite(144, 570, 'candy');
