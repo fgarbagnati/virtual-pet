e6e4441e67f3a1b6563b0dfaf8b74c560ce29f85
Francesca Garbagnati <f.g.garbagnati@gmail.com>
2017-01-23 12:41:23 -0800

    Adds images and spritesheet to preload()

--- public/js/main.js
+++ public/js/main.js
@@ -7,7 +7,13 @@ var GameState = {
 
 	},
 	preload: function() {
-
+		this.load.image('backyard', 'assets/images/backyard.png');
+		this.load.image('apple', 'assets/images/apple.png');
+		this.load.image('candy', 'assets/images/candy.png');
+		this.load.image('rotate', 'assets/images/rotate.png');
+		this.load.image('toy', 'assets/images/rubber_duck.png');
+		this.load.image('arrow', 'assets/images/arrow.png');
+		this.load.spritesheet('pet', 'assets/images/pet.png', 97, 83, 5, 1, 1);
 	},
 	create: function() {
 
