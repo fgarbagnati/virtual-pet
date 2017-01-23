1e8d26347d7a5d0b075b25b1173639d60aceee63
Francesca Garbagnati <f.g.garbagnati@gmail.com>
2017-01-23 12:37:24 -0800

    Sets up alignment

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,9 @@
 var GameState = {
 	init: function() {
+		this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+
+		this.scale.pageAlignHorizontally = true;
+		this.scale.pageAlignVertically = true;
 
 	},
 	preload: function() {
@@ -13,4 +17,4 @@ var GameState = {
 var game = new Phaser.Game(360, 640, Phaser.AUTO);
 
 game.state.add('GameState', GameState);
-game.state.start('GameState');
+game.state.start('GameState'); 
