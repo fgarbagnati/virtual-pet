406edafaf4e91a371ba732dfca88d144dff33533
Francesca Garbagnati <f.g.garbagnati@gmail.com>
2017-01-23 12:52:36 -0800

    Places sprites on game in approximate locations

--- public/js/main.js
+++ public/js/main.js
@@ -16,6 +16,16 @@ var GameState = {
 		this.load.spritesheet('pet', 'assets/images/pet.png', 97, 83, 5, 1, 1);
 	},
 	create: function() {
+		this.background = this.game.add.sprite(0, 0, 'backyard');
+
+		this.pet = this.game.add.sprite(100, 400, 'pet');
+		this.pet.anchor.setTo(0.5);
+		this.pet.customParams = {health: 100, fun: 100};
+
+		this.apple = this.game.add.sprite(72, 570, 'apple');
+		this.candy = this.game.add.sprite(144, 570, 'candy');
+		this.toy = this.game.add.sprite(216, 570, 'toy');
+		this.rotate = this.game.add.sprite(288, 570, 'rotate');
 
 	}
 };
