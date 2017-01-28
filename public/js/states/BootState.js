596e9455a10cf3c2048d4ddc87e8c170e78b90bf
Francesca Garbagnati <f.g.garbagnati@gmail.com>
2017-01-28 13:37:10 -0800

    Changes entry point to game to BootState, then directs to PreloadState

--- public/js/states/BootState.js
+++ public/js/states/BootState.js
@@ -8,5 +8,10 @@ var BootState = {
 	preload: function() {
 		this.load.images('preloadBar', 'assets/images/bar.png');
 		this.load.images('logo', 'assets/images/logo.png');
+	},
+	create: function() {
+		this.game.stage.backgroundColor = '#fff';
+
+		this.state.start('PreloadState');
 	}
 };
