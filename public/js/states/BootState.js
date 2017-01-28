d75f8927d5130aa149d21acac2f9d85b5dfc79ea
Francesca Garbagnati <f.g.garbagnati@gmail.com>
2017-01-28 13:36:12 -0800

    Adds logo image and loading bar image and loads them in BootState

--- public/js/states/BootState.js
+++ public/js/states/BootState.js
@@ -1,3 +1,12 @@
 var BootState = {
-	
+	init: function() {
+		this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+
+		this.scale.pageAlignHorizontally = true;
+		this.scale.pageAlignVertically = true;
+	},
+	preload: function() {
+		this.load.images('preloadBar', 'assets/images/bar.png');
+		this.load.images('logo', 'assets/images/logo.png');
+	}
 };
