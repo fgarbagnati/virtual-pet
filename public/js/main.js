b72c43bb864a97a84e4139ba46f1e348aca6469c
Francesca Garbagnati <f.g.garbagnati@gmail.com>
2017-01-28 03:16:55 -0800

    Adds states to main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,7 @@
 var game = new Phaser.Game(360, 640, Phaser.AUTO);
 
 game.state.add('GameState', GameState);
+game.state.add('BootState', BootState);
+game.state.add('HomeState', HomeState);
+game.state.add('PreloadState', PreloadState);
 game.state.start('GameState'); 
